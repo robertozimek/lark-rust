2b006adf11a7aa1b0466b0534a295180b6d2234c
Robert Ozimek <robertozimek90@gmail.com>
2026-03-18 22:44:49 -0400

    feat: add LALR parser with ParseTable, ParseConf, ParserState, Tree, Meta

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,19 @@ mod lexer;
 mod lexer_state;
 mod lexer_thread;
 mod line_counter;
+mod parser;
 mod scanner;
 mod token;
+mod tree;
+
 use lexer::BasicLexer;
 use lexer_state::LexerState;
 use lexer_thread::LexerThread;
 use line_counter::LineCounter;
+use parser::{LALR_Parser, ParseConf, ParseTable, ParserState, Rule};
 use scanner::Scanner;
 use token::Token;
+use tree::{Meta, Tree};
 
 #[pymodule]
 fn lark_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -21,5 +26,12 @@ fn lark_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Scanner>()?;
     m.add_class::<BasicLexer>()?;
     m.add_class::<LexerThread>()?;
+    m.add_class::<Rule>()?;
+    m.add_class::<ParseTable>()?;
+    m.add_class::<ParseConf>()?;
+    m.add_class::<ParserState>()?;
+    m.add_class::<LALR_Parser>()?;
+    m.add_class::<Meta>()?;
+    m.add_class::<Tree>()?;
     Ok(())
 }
